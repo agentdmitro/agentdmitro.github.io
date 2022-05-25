c23cb4cca86694e0d69a2f0b2c6344cd4ea15b63
Дмитрий <76614675+agentdmitro@users.noreply.github.com>
2022-05-25 15:12:37 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// Vue.component('project', {
+//     props: ['project'],
+//     template: `
+//     <div class="project__box">
+//         <a :href=" project.link " target="_blank">
+//             <div class="project__box-icon"><img class="project__box-img" :src="'img/projects/' + project.img + '.png'" :alt="project.title" loading="lazy"></div>
+//             <div class="project__box-info">
+//                 <div class="project__box-title">{{project.title}}</div>
+//                 <div class="project__box-date">Добавлено: <span>{{project.date}}</span></div>
+//             </div>
+//         </a>
+//     </div>
+// `
+// })
+
+// const app = new Vue({
+// 	// router
+// 	data: {
+// 		projects: [
+// 			{ title: 'Курс по сборке ПК', img: 'course', link: 'https://agentdmitro.github.io/Course/', date: '13.04.2021'},
+// 			{ title: 'IT-Meteor', img: 'meteor', link: 'https://it-meteor.ru/', date: '04.06.2021' },
+// 			{ title: 'skinTrade', img: 'skintrade', link: 'https://agentdmitro.github.io/SkinTrade/', date: '29.03.2021' },
+// 			{ title: 'Gravity', img: 'gravity', link: 'https://agentdmitro.github.io/Gravity', date: '30.03.2021' },
+// 			{ title: 'I-Lights', img: 'iLights', link: 'https://agentdmitro.github.io/I-Lights', date: '24.03.2021' },
+// 			{ title: 'instaBot', img: 'instaBot', link: 'https://agentdmitro.github.io/InstaBOT', date: '08.04.2021' },
+// 			{ title: 'LaslesVPN', img: 'lasles', link: 'https://agentdmitro.github.io/LaslesVPN', date: '10.03.2021' },
+// 			{ title: 'AllBirds', img: 'allbirds', link: 'https://agentdmitro.github.io/AllBirds', date: '20.03.2021' },
+// 			{ title: 'Crappo', img: 'crappo', link: 'https://agentdmitro.github.io/CRAPPO/', date: '08.03.2021' },
+// 			{ title: 'SushiBox', img: 'suchibox', link: 'https://agentdmitro.github.io/SuchiBox', date: '18.03.2021' },
+// 			{ title: 'Mafia', img: 'mafia', link: 'https://agentdmitro.github.io/mafia', date: '23.04.2021' },
+// 			{ title: 'Design agency', img: 'designAgency', link: 'https://agentdmitro.github.io/DesignAgency', date: '15.03.2021' },
+
+// 		]
+// 	}
+// }).$mount('#app')
+
